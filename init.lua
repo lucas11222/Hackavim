d66d562fd2c403a178d8b1082b43bdf3736cef36
BuildTools <altamiranolucas2013@gmail.com>
2025-08-25 21:51:13 +0200

    please work x2

--- init.lua
+++ init.lua
@@ -3,7 +3,9 @@ function M.hello_world()
   print("Hello, World!")
 end
 
-vim.api.nvim_create_user_command("Hackavim", function()
-  M.hello_world()
-end, {})
+vim.api.nvim_create_user_command('BufOnly',function()
+  pcall(function()
+    M.hello_world()
+  end)
+end,{})
 return M
