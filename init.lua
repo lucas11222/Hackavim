6e733b5c8f709dd41d7c5da1abdceb92efe1731e
BuildTools <altamiranolucas2013@gmail.com>
2025-08-25 18:32:45 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ function M.hello_world()
 end
 
 -- Map a command to the function
-vim.api.nvim_command('command! hackatime lua require("hackavim").hello_world()')
-
+vim.api.nvim_create_user_command(
+  "Hackatime",                     -- command name (must start uppercase)
+  function()
+    require("Hackavim").hello_world()  -- call your Lua function
+  end,
+  {}                               -- options
+)
 return M
